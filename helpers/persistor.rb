7ad9f60e3cb4bbc0d91088b674c9bfd617c3fd60
Jing Li <thyrlian@gmail.com>
2014-09-30 14:21:59 +0200

    Added global Persistor helper class.

---
+++ helpers/persistor.rb
@@ -0,0 +1,22 @@
+module CaptainADB
+  class Persistor
+    class << self
+      def get_var(name)
+        # class_variable_get("@@#{name.to_s}".intern)
+        if list_vars.include?(name.intern)
+          class_variable_get("@@#{name.to_s}".intern)
+        else
+          raise(RuntimeError, "No such variable defined: #{name.to_s}", caller)
+        end
+      end
+      
+      def set_var(name, value)
+        class_variable_set("@@#{name.to_s}".intern, value)
+      end
+      
+      def list_vars
+        class_variables.map { |var| var.to_s.gsub(/@@/, '').intern }
+      end
+    end
+  end
+end
